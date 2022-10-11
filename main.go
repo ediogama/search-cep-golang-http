5806c36b7b239ffd793efe7cd26ed5d81f16666f
Edio Gama <ediogama@hotmail.com>
2022-10-10 21:38:23 -0300

    add hello world

--- main.go
+++ main.go
@@ -1,3 +1,12 @@
 package main
 
-func main() {}
+import "net/http"
+
+func main() {
+	http.HandleFunc("/", SearchCEP)
+	http.ListenAndServe(":8080", nil)
+}
+
+func SearchCEP(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello, World!"))
+}
